74d72750ae286a301ff7db825d11fcff48a3dd6f
sabila111 <sharminsharker2002@gmail.com>
2024-11-21 02:15:11 +0600

    finished brand details page

--- src/components/brands/BrandDetails.jsx
+++ src/components/brands/BrandDetails.jsx
@@ -7,9 +7,22 @@ const BrandDetails = () => {
     const brand = brands.find((brand) => brand._id === id)
     return (
         <div>
-            <h2>{brand.brand_name}</h2>
-            
-          <div>
+            <img className="w-3/6 h-56 mt-10 mx-auto" src={brand.brand_logo} alt="" />
+            <p className="text-4xl font-extrabold my-5 text-center">{brand.brand_name} </p>
+
+            <div className="rating flex justify-center items-center">
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input
+                        type="radio"
+                        name="rating-2"
+                        className="mask mask-star-2 bg-orange-400"
+                    />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked />
+                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                </div>
+
+          <div className="my-5">
           {
             brand.coupons.map((brand) => <BrandDetailsCard key={brand} brand={brand}></BrandDetailsCard>)
            }
--- src/components/brands/BrandDetailsCard.jsx
+++ src/components/brands/BrandDetailsCard.jsx
@@ -1,21 +1,33 @@
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const BrandDetailsCard = ({brand}) => {
-    const { coupon_code, description, expiry_date,condition, coupon_type} = brand
-    return (
-        <div className="card bg-base-100 w-96 shadow-xl">
-  <div className="">
-    <h2 className="card-title">Code:{coupon_code}</h2>
-    <p>{description}</p>
-    <p>{expiry_date}</p>
-    <p>{condition}</p>
-    <p>{coupon_type}</p>
+    const { coupon_code,shop_Link, description, expiry_date,condition, coupon_type} = brand
 
-    <div className="flex justify-around">
-      <button className="text-xl font-semibold px-3 py-4">Copy Code</button>
-      <button className="text-xl font-semibold px-3 py-4">use Now</button>
+    const handleCopy = (code) => {
+        toast.success(`Copied "${code}" to clipboard!`);
+      };
+
+    return (
+        <div className="card mx-auto border-2 border-sky-700 bg-sky-200 p-7 my-10 w-96 shadow-xl">
+  <div className="">
+    <h2 className="text-2xl font-bold mb-2">{description}</h2>
+    <p className="text-xl font-semibold my-3">Code: {coupon_code}</p>
+    <p className="text-lg font-normal">Expire Date: {expiry_date}</p>
+    <p className="text-lg font-normal">Condition: {condition}</p>
+    <p className="text-lg font-normal">Coupon type: {coupon_type}</p>
+
+    <div className="flex justify-between my-5">
+
+     <CopyToClipboard text={coupon_code} onCopy={()=> handleCopy(coupon_code)}>
+     <button className="text-xl font-semibold px-4 py-4 bg-sky-700 border-2 border-sky-700 rounded-xl text-white">Copy Code</button>
+     </CopyToClipboard>
+
+      <button onClick={()=> window.open(shop_Link, 'blank')} className="text-xl font-semibold px-4 py-3 bg-white text-sky-700 rounded-xl border-2 border-sky-700">use Now</button>
     </div>
   </div>
+  <ToastContainer />
 </div>
     );
 };
